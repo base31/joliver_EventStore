539dcf7e511be0283d307b816d60cc8fd0746d34
db <dev@base31.com>
2013-01-31 12:22:17 +0100

    modified wireup constructs

--- src/proj/EventStore.Persistence.ElasticSearchPersistence.Wireup/ElasticSearchPersistenceWireup.cs
+++ src/proj/EventStore.Persistence.ElasticSearchPersistence.Wireup/ElasticSearchPersistenceWireup.cs
@@ -1,14 +1,27 @@
-﻿using EventStore.Logging;
+﻿using System.Transactions;
+using EventStore.Logging;
+using EventStore.Persistence.ElasticSearchPersistence;
+using EventStore.Serialization;
 
 namespace EventStore
 {
     public class ElasticSearchPersistenceWireup : PersistenceWireup
     {
-        private static readonly ILog Logger = LogFactory.BuildLogger(typeof(ElasticSearchPersistenceWireup));
-        
-        public ElasticSearchPersistenceWireup(Wireup inner):base(inner)
+        private static readonly ILog Logger = LogFactory.BuildLogger(typeof (ElasticSearchPersistenceWireup));
+
+        public ElasticSearchPersistenceWireup(Wireup inner, IElasticSearchPersistenceConfiguration configuration,
+                                              IDocumentSerializer documentSerializer)
+            : base(inner)
         {
-            
+            Logger.Debug("Configuring ElasticSearch persistence engine.");
+
+            var options = Container.Resolve<TransactionScopeOption>();
+            if (options != TransactionScopeOption.Suppress)
+                Logger.Warn("ElasticSearch does not participate in transactions using TransactionScope.");
+
+            Container.Register(c => new ElasticSearchPersistenceFactory(
+                                        configuration,
+                                        documentSerializer).Build());
         }
     }
 }
--- src/proj/EventStore.Persistence.ElasticSearchPersistence.Wireup/ElasticSearchPersistenceWireupExtensions.cs
+++ src/proj/EventStore.Persistence.ElasticSearchPersistence.Wireup/ElasticSearchPersistenceWireupExtensions.cs
@@ -1,6 +1,15 @@
-﻿namespace EventStore.Persistence.ElasticSearchPersistence.Wireup
+﻿using EventStore.Persistence.ElasticSearchPersistence;
+using EventStore.Serialization;
+
+namespace EventStore
 {
-    public class ElasticSearchPersistenceWireupExtensions
+    public static class ElasticSearchPersistenceWireupExtensions
     {
+        public static ElasticSearchPersistenceWireup UsingElasticSearchPersistence(this Wireup inner,
+                                                                      IElasticSearchPersistenceConfiguration configuration,
+                                                                      IDocumentSerializer documentSerializer)
+        {
+            return new ElasticSearchPersistenceWireup(inner, configuration, documentSerializer);
+        }
     }
 }
